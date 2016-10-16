8b2c803b4d680abced2352269b494c2221b9f9ef
o0 <o0@coma.mbase.int>
2016-10-16 01:04:15 -0400

    readded nsfw tagging

--- static/general.js
+++ static/general.js
@@ -957,6 +957,41 @@ function loadRights() {
     xhr.send();
 }
 
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function setNSFW() {
+    var nsfw = readCookie("show_nsfw");
+    if (nsfw === "true") {
+        qs(document, "input#nsfw_checkbox").setAttribute("checked", "true");
+    }
+}
+
+function toggleNSFW(e) {
+    var nsfw = (e.target.hasAttribute("checked")) ? "false" : "true";
+    createCookie("show_nsfw", nsfw);
+    document.location.reload(true);
+}
+
 function main() {
     defineGlobals();
 
@@ -969,6 +1004,8 @@ function main() {
     }
 
     addCommentHandlers();
+    setNSFW();
+    qsael(document, "input#nsfw_checkbox", "click", toggleNSFW, false);
     qsael(document, "input#tag_search", "focus", loadAutoComplete, false);
     loadRights();
 }
